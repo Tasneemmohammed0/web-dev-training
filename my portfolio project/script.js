493a161db21a04f14b7050c0e3a76d120541797f
Tasneem Mohammed <tasneemmohammed098@gmail.com>
2024-08-26 15:42:52 +0200

    📲 Finished responsive design of the header

--- my portfolio project/script.js
+++ my portfolio project/script.js
@@ -1,13 +1,22 @@
 "use strict";
 
 const header = document.querySelector(".header");
-let lastScrollY = window.scrollY;
+const menuIcon = document.querySelector(".menu-icon");
+const nav = document.querySelector(".nav");
 
+// Responsive design of nav items
+menuIcon.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
+
+// Header appearance
+let lastScrollY = window.scrollY;
 window.addEventListener("scroll", () => {
   if (window.scrollY < lastScrollY) {
     header.classList.add("sticky");
   } else {
     header.classList.remove("sticky");
   }
+  nav.classList.remove("active");
   lastScrollY = window.scrollY;
 });
